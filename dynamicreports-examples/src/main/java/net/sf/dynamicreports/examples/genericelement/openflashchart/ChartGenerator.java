1836aaca60ffdf810b33975c735bc3d4cbed7255
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2012-02-01 08:51:48 +0000

    open flash chart example
    git-svn-id: http://svn.code.sf.net/p/dynamicreports/code/trunk@426 bf0c503b-2351-4f66-9eae-aa52f3279a3f

---
+++ dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/genericelement/openflashchart/ChartGenerator.java
@@ -0,0 +1,8 @@
+package net.sf.dynamicreports.examples.genericelement.openflashchart;
+
+public interface ChartGenerator {
+	public static final String PARAMETER_CHART_GENERATOR = "CHARTGENERATOR";
+
+	public String generateChart();
+
+}
---
+++ dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/genericelement/openflashchart/OpenFlashChartReport.java
@@ -0,0 +1,103 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.examples.genericelement.openflashchart;
+
+import static net.sf.dynamicreports.report.builder.DynamicReports.*;
+
+import java.io.FileOutputStream;
+import java.math.BigDecimal;
+
+import net.sf.dynamicreports.examples.DataSource;
+import net.sf.dynamicreports.examples.Templates;
+import net.sf.dynamicreports.report.base.AbstractScriptlet;
+import net.sf.dynamicreports.report.builder.component.GenericElementBuilder;
+import net.sf.dynamicreports.report.definition.ReportParameters;
+import net.sf.jasperreports.engine.JRDataSource;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public class OpenFlashChartReport {
+	private PieChart pieChart1;
+	private PieChart pieChart2;
+
+	public OpenFlashChartReport() {
+		build();
+	}
+
+	private void build() {
+		try {
+			pieChart1 = new PieChart();
+			pieChart1.setTitle("Quantity");
+			GenericElementBuilder chart1 = cmp.genericElement("http://dynamicreports.sourceforge.net/openflashchart", "openflashchart")
+			  .setHeight(200)
+			  .addParameter(PieChart.PARAMETER_CHART_GENERATOR, pieChart1);
+
+			pieChart2 = new PieChart();
+			pieChart2.setTitle("Unit price");
+			GenericElementBuilder chart2 = cmp.genericElement("http://dynamicreports.sourceforge.net/openflashchart", "openflashchart")
+			  .setHeight(200)
+			  .addParameter(PieChart.PARAMETER_CHART_GENERATOR, pieChart2);
+
+			report()
+			  .setTemplate(Templates.reportTemplate)
+			  .scriptlets(new ReportScriptlet())
+			  .columns(
+					col.column("Item", "item", type.stringType()),
+					col.column("Quantity", "quantity", type.integerType()),
+					col.column("Unit price", "unitprice", type.bigDecimalType()))
+			  .title(Templates.createTitleComponent("OpenFlashChart"))
+			  .summary(
+			  	cmp.horizontalList(chart1, chart2))
+			  .setDataSource(createDataSource())
+			  .toPdf(new FileOutputStream("c:/report.pdf"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected JRDataSource createDataSource() {
+		DataSource dataSource = new DataSource("item", "quantity", "unitprice");
+		dataSource.add("Book", 60, new BigDecimal(150));
+		dataSource.add("Camera", 40, new BigDecimal(400));
+		dataSource.add("PDA", 50, new BigDecimal(250));
+		return dataSource;
+	}
+
+	private class ReportScriptlet extends AbstractScriptlet {
+
+		@Override
+		public void afterDetailEval(ReportParameters reportParameters) {
+			super.afterDetailEval(reportParameters);
+			String item = reportParameters.getValue("item");
+			Integer quantity = reportParameters.getValue("quantity");
+			BigDecimal unitPrice = reportParameters.getValue("unitprice");
+			pieChart1.addValue(item, quantity);
+			pieChart2.addValue(item, unitPrice);
+		}
+	}
+
+	public static void main(String[] args) {
+		new OpenFlashChartReport();
+	}
+}
