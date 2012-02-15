72b190e0d33ae3f2d0dde5b7b232fc99ed2055a5
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2012-02-15 09:27:30 +0000

    minor changes
    git-svn-id: http://svn.code.sf.net/p/dynamicreports/code/trunk@438 bf0c503b-2351-4f66-9eae-aa52f3279a3f

--- dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/genericelement/openflashchart/OpenFlashChartXhtmlHandler.java
+++ dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/genericelement/openflashchart/OpenFlashChartXhtmlHandler.java
@@ -1,7 +1,7 @@
 /**
  * DynamicReports - Free Java reporting library for creating reports dynamically
  *
- * Copyright (C) 2010 Ricardo Mariaca
+ * Copyright (C) 2010 - 2012 Ricardo Mariaca
  * http://dynamicreports.sourceforge.net
  *
  * This file is part of DynamicReports.
