8c1337eb9e03e860e9e13dded0b23c7294b7e49d
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2012-03-07 10:16:55 +0000

    renamed
    git-svn-id: http://svn.code.sf.net/p/dynamicreports/code/trunk@461 bf0c503b-2351-4f66-9eae-aa52f3279a3f

--- dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/tableofcontents/TableOfContentsReport1.java
+++ dynamicreports-examples/src/main/java/net/sf/dynamicreports/examples/tableofcontents/TableOfContentsReport1.java
@@ -20,61 +20,76 @@
  * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package net.sf.dynamicreports.examples.exporter;
+package net.sf.dynamicreports.examples.tableofcontents;
 
 import static net.sf.dynamicreports.report.builder.DynamicReports.*;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 
 import net.sf.dynamicreports.examples.DataSource;
 import net.sf.dynamicreports.examples.Templates;
-import net.sf.dynamicreports.jasper.builder.export.JasperXlsExporterBuilder;
-import net.sf.dynamicreports.jasper.constant.JasperProperty;
+import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
 import net.sf.dynamicreports.report.exception.DRException;
 import net.sf.jasperreports.engine.JRDataSource;
 
 /**
  * @author Ricardo Mariaca (dynamicreports@gmail.com)
  */
-public class ExcelReport {
+public class TableOfContentsReport1 {
 
-	public ExcelReport() {
+	public TableOfContentsReport1() {
 		build();
 	}
 
 	private void build() {
-		try {
-			JasperXlsExporterBuilder xlsExporter = export.xlsExporter("c:/report.xls")
-			                                             .setDetectCellType(true)
-			                                             .setIgnorePageMargins(true)
-			                                             .setWhitePageBackground(false)
-			                                             .setRemoveEmptySpaceBetweenColumns(true);
+		TextColumnBuilder<String> countryColumn = col.column("Country", "country", type.stringType());
+		TextColumnBuilder<String> itemColumn    = col.column("Item",    "item",    type.stringType());
 
+		try {
 			report()
-			  .setColumnTitleStyle(Templates.columnTitleStyle)
-			  .addProperty(JasperProperty.EXPORT_XLS_FREEZE_ROW, "2")
-			  .ignorePageWidth()
-			  .ignorePagination()
+			  .setTemplate(Templates.reportTemplate)
+			  .tableOfContents()
 			  .columns(
-			  	col.column("Item",       "item",      type.stringType()),
+			  	countryColumn,
+			  	itemColumn,
+			  	col.column("Order date", "orderdate", type.dateType()),
 			  	col.column("Quantity",   "quantity",  type.integerType()),
 			  	col.column("Unit price", "unitprice", type.bigDecimalType()))
+			  .groupBy(countryColumn, itemColumn)
+			  .title(Templates.createTitleComponent("TableOfContents1"))
+			  .pageFooter(Templates.footerComponent)
 			  .setDataSource(createDataSource())
-			  .toXls(xlsExporter);
+			  .show();
 		} catch (DRException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private JRDataSource createDataSource() {
-		DataSource dataSource = new DataSource("item", "quantity", "unitprice");
-		for (int i = 0; i < 50; i++) {
-			dataSource.add("Book", (int) (Math.random() * 10) + 1, new BigDecimal(Math.random() * 100 + 1));
+		String[] countries = new String[]{"USA", "Canada", "Mexico"};
+		String[] items = new String[]{"Book", "Notebook", "PDA"};
+		DataSource dataSource = new DataSource("country", "item", "orderdate", "quantity", "unitprice");
+		for (String country : countries) {
+			for (String item : items) {
+				for (int i = 0; i < 8; i++) {
+					dataSource.add(country, item, toDate(2010, 1, (int) (Math.random() * 10) + 1), (int) (Math.random() * 10) + 1, new BigDecimal(Math.random() * 100 + 1));
+				}
+			}
 		}
 		return dataSource;
 	}
 
+	private Date toDate(int year, int month, int day) {
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, month - 1);
+		c.set(Calendar.DAY_OF_MONTH, day);
+		return c.getTime();
+	}
+
 	public static void main(String[] args) {
-		new ExcelReport();
+		new TableOfContentsReport1();
 	}
 }
