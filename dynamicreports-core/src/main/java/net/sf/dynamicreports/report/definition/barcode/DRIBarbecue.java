ff4d1d235148c616e9210aecc87f07099ec38b9d
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2014-02-14 07:59:17 +0000

    updated license
    git-svn-id: http://svn.code.sf.net/p/dynamicreports/code/trunk@776 bf0c503b-2351-4f66-9eae-aa52f3279a3f

--- dynamicreports-core/src/main/java/net/sf/dynamicreports/report/definition/barcode/DRIBarbecue.java
+++ dynamicreports-core/src/main/java/net/sf/dynamicreports/report/definition/barcode/DRIBarbecue.java
@@ -1,7 +1,7 @@
 /**
  * DynamicReports - Free Java reporting library for creating reports dynamically
  *
- * Copyright (C) 2010 - 2013 Ricardo Mariaca
+ * Copyright (C) 2010 - 2014 Ricardo Mariaca
  * http://www.dynamicreports.org
  *
  * This file is part of DynamicReports.
